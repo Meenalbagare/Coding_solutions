2e7a63285420e1d8443135a6db0252a73b8af49b
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2023-10-14 11:08:35 +0530

    Time: 116 ms (62.83%), Space: 17.3 MB (23.44%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -5,18 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        vowels = {'a', 'e', 'i', 'o', 'u'}  # Set of vowel letters
+        vowels = {'a', 'e', 'i', 'o', 'u'}  
         max_count = 0
         count = 0
 
         for i in range(len(s)):
             if s[i] in vowels:
-                count += 1  # Increment count if the current character is a vowel
+                count += 1 
 
             if i >= k:
                 if s[i - k] in vowels:
-                    count -= 1  # Decrement count if the character leaving the window is a vowel
+                    count -= 1  
 
-            max_count = max(max_count, count)  # Update max_count with the maximum count found so far
+            max_count = max(max_count, count) 
 
         return max_count
