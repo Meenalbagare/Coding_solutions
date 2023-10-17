d007e970612193696484cad648c522f589de1bcb
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2023-10-17 20:30:48 +0530

    Time: 362 ms (23.44%), Space: 14.9 MB (94.91%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -8,8 +8,6 @@ class Solution(object):
         
         c1=Counter(word1)
         c2=Counter(word2)
-       # n1=Counter([i for i in c1.values()])
-       # n2=Counter([j for j in c2.values()])
         n1=Counter(c1.values())
         n2=Counter(c2.values())
         return c1==c2 or(n1==n2 and set(word1)==set(word2))
