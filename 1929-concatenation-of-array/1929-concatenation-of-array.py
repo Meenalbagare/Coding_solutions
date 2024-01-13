7b20b9094a0cd8a607c490f202db7516ab9912fb
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-01-14 00:26:50 +0530

    Time: 60 ms (28.77%), Space: 13.5 MB (69.85%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n=len(nums)
-        ans1=[0]*(2*n)
-        for i in range(n):
-            ans1[i]=nums[i]
-            ans1[i+n]=nums[i]
-        return ans1
+        # n=len(nums)
+        # ans1=[0]*(2*n)
+        # for i in range(n):
+        #     ans1[i]=nums[i]
+        #     ans1[i+n]=nums[i]
+        # return ans1
+        
+        return nums+nums
             
         
