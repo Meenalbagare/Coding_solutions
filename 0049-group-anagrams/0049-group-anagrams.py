b8def694049c38e2fc6f3a323d69ae016709fb0c
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-02-06 15:30:53 +0530

    Time: 88 ms (72.13%), Space: 19.7 MB (88.32%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        dictionary=defaultdict(list)
-        for word in strs:
-            count=[0]*26
-            for char in word:
-                count[ord(char)-ord("a")]+=1
-            dictionary[tuple(count)].append(word)
-        return dictionary.values()
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)
+        for i in strs:
+            sorted_word=''.join(sorted(i))
+            res[sorted_word].append(i)
+        return list(res.values())
