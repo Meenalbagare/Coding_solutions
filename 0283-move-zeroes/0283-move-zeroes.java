0740a46c6ef0afc254aa91655082298c6e5615ae
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-02-13 16:08:04 +0530

    Time: 90 ms (5.17%), Space: 46.2 MB (41.38%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int j=i+1;
+            while(nums[i]==0 && j<nums.length){
+                if(nums[j]!=0){
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        // return nums;
+    }
+}
