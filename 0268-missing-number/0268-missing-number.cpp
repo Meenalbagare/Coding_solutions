c39456cde720e5f65af4beb163e05c90234e5c8a
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-02-20 22:58:57 +0530

    Time: 17 ms (37.16%), Space: 21 MB (10.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,15 @@
-import java.util.List;
 class Solution {
-    public int missingNumber(int[] nums) {
-        int[] v=new int[(nums.length)+1];
-        Arrays.fill(v,-1);
-        for(int i=0;i<nums.length;i++){
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<int> v((nums.size())+1,-1);
+        for(int i=0;i<nums.size();i++){
             v[nums[i]]=nums[i];
         }
-        for(int j=0;j<v.length;j++){
-            if(v[j]==-1){
-                return j;
+        for(int k=0;k<v.size();k++){
+            if(v[k]==-1){
+                return k;
             }
         }
         return 0;
-        
     }
-}
- 
+};
