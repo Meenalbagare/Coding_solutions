ca40b1ca8a83a7b1b69514947eecd70cecca8dec
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-03-08 21:56:53 +0530

    Time: 2 ms (70.74%), Space: 41.7 MB (98.78%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -1,15 +1,18 @@
-class Solution:
-    def maxFrequencyElements(self, nums: List[int]) -> int:
-        d={}
-        c=0
-        for i in range(len(nums)):
-            if nums[i] not in d:
-                d[nums[i]]=1
-            else:
-                d[nums[i]]+=1
-        maxi=max(d.values())
-        for i,j in d.items():
-            if j==maxi:
-                c+=j
-        return c
-                
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer,Integer> hash=new HashMap<>();
+        int c=0;
+        int maxi=0;
+        for(int i:nums){
+            hash.put(i,hash.getOrDefault(i,0)+1);
+            maxi=Math.max(maxi,hash.get(i));
+        }
+        for(int j:hash.values()){
+            if(j==maxi){
+                c+=j;
+            }
+        }
+        return c;
+    }
+}
+
