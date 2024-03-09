dc35186dfd97e7fa9e4412af238e52c1ae113703
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-03-09 12:23:04 +0530

    Time: 2 ms (81.56%), Space: 61.7 MB (99.04%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,15 +1,19 @@
-class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        l=0
-        r=0
-        l1=len(nums1)
-        l2=len(nums2)
-        while (l<l1 and r<l2):
-            if nums1[l]==nums2[r]:
-                return nums1[l]
-            elif nums1[l]<nums2[r]:
-                l+=1
-            else:
-                r+=1
-        return -1
-            
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int l=0,r=0;
+        int l1=nums1.length;
+        int l2=nums2.length;
+        while (l<l1 && r<l2){
+            if(nums1[l]==nums2[r]){
+                return nums1[l];
+            }
+            else if(nums1[l]<nums2[r]){
+                l++;
+            }
+            else{
+                r++;
+            }
+        }
+        return -1;
+    }
+}
