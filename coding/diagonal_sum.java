10b3cfa4696a7f625b9e88d490ffc3af7f544015
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-03-17 20:40:34 +0530

    Add files via upload

---
+++ coding/diagonal_sum.java
@@ -0,0 +1,27 @@
+//SPIRAL MATRIX
+import java.*;
+//DIAGONAL SUM
+public class Solution {
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+        System.out.println(diagonalsum(matrix));
+    }
+    public static int diagonalsum(int matrix[][]){
+        int primarydiagonal = 0;
+        int secondaydiagonal=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if (i==j){
+                   primarydiagonal+=matrix[i][j];
+                }
+                if(i+j==matrix.length-1){
+                    secondaydiagonal+=matrix[i][j];
+                }
+            }
+        }
+        return secondaydiagonal+primarydiagonal;
+    }
+}
