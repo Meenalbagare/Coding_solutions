4f927d5ff13cabb21a0256ba35e237e0dd1ad12e
Meenalbagare <99323366+Meenalbagare@users.noreply.github.com>
2024-03-26 08:30:34 +0530

    Time: 80 ms (15.16%), Space: 60.8 MB (6.19%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,12 +1,15 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums) set.add(num);
-        int i=1;
-        while(i<=nums.length){
-            if(set.contains(i)) i++;
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> set;
+
+        for (int num : nums)    set.insert(num);
+
+        int i = 1;
+        while (i <= nums.size()) {
+            if (set.find(i) != set.end())   i++;
             else    return i;
         }
-        return nums.length+1;
+        return nums.size() + 1;
     }
-}
+};
