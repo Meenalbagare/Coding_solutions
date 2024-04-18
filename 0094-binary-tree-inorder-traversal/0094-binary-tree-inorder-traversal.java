0c37aae4999666f4b8d791bf3fc0f2ba9a8fdeaa
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-04-18 23:09:07 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (22.79%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,19 +1,32 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res=[]
-        def inorder(root):
-            if root:
-                inorder(root.left)
-                res.append(root.val)
-
-                inorder(root.right)
-
-        inorder(root)
-        return res
-        
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        return inorder(root,res);
+    }
+    private List<Integer> inorder(TreeNode root,List<Integer> res){
+        if(root!=null){
+            inorder(root.left,res);
+            res.add(root.val);
+            inorder(root.right,res);
+        }
+        return res;
+    }
+}
