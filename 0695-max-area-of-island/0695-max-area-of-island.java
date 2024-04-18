2524dfeaf5c53cee154249f063eb529848af83a1
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-04-18 10:42:40 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (76.53%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -4,7 +4,7 @@ class Solution {
         int m=grid[0].length;
         if (i>=0 && i<n && j>=0 && j<m && grid[i][j]==1){
             grid[i][j]=0;
-            return (1+dfs(grid,i-1,j,t)+dfs(grid,i+1,j,t)+dfs(grid,i,j+1,t)+dfs(grid,i,j-1,t));
+            return (1+dfs(grid,i-1,j,t)+dfs(grid,i,j-1,t)+dfs(grid,i+1,j,t)+dfs(grid,i,j+1,t));
         }
         return 0;
     }
