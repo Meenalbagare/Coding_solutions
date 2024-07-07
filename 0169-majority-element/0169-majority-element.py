985158cf55b72b2b999dd56412584ea709aa270b
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-07-07 12:19:34 +0530

    Time: 169 ms (63.89%), Space: 17.9 MB (53.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,35 +1,7 @@
-class Solution {
-    
-    public static int counting(int[] nums,int num,int s, int e) {
-        int c=0;
-        for (int i=s;i<=e;i++) {
-            if (nums[i] ==num){
-                c++;
-            }
-        }
-        return c;
-    }
-    
-    public static int majorityEleRec(int[] nums,int s,int e) {
-        if (s==e) {
-            return nums[s];
-        }
-        
-        int mid = s+ (e-s)/2;
-        int l = majorityEleRec(nums,s,mid);
-        int r = majorityEleRec(nums,mid+1,e);
-        
-        if (l==r) {
-            return l;
-        }
-        
-        int lcount = counting(nums,l,s,e);
-        int rcount = counting(nums,r,s,e);
-        
-        return lcount>rcount ? l : r;
-        
-    }
-    public int majorityElement(int[] nums) {
-        return majorityEleRec(nums,0,nums.length-1);
-    }
-}
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        n=len(nums)
+        return nums[n//2]
+            
+            
