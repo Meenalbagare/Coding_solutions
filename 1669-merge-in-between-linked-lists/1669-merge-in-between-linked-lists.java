46723a045826b086f5277c46ee616da7958d7abc
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-08-03 16:34:44 +0530

    Time: 1 ms (100.00%), Space: 46.2 MB (88.97%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -10,29 +10,27 @@
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode currNode = list1; // Start of the first list
-        int index = 0; // Variable to track current position
-        // Move to the element before index "a"
-        while (index < a - 1) {
-            currNode = currNode.next;
-            index++;
+       
+        ListNode cur=list1;
+        int i=0;
+        while(i<a-1){
+            cur=cur.next;
+            i++;
         }
-        ListNode front = currNode; // Store current position as "front"
-        // Move to the element after index "b"
-        while (index < b + 1) {
-            currNode = currNode.next;
-            index++;
+        //add list2 here
+        ListNode head2=list2;
+        ListNode rear=cur;
+        
+        while(i<b+1){
+            rear=rear.next;
+            i++;
         }
-        ListNode rear = currNode; // Store current position as "rear"
-        ListNode secondListTail = list2, secondListHead = list2;
-        // Finding the tail of the second list
-        while (secondListTail.next != null) {
-            secondListTail = secondListTail.next;
+        cur.next=head2;
+        while(cur.next!=null){
+            cur=cur.next;
         }
-        // Link the front part of the second list after "front"
-        front.next = secondListHead;
-        // Link the rear part of the second list before "rear"
-        secondListTail.next = rear;
-        return list1; // Return the updated list
+        // System.out.println(rear.val);
+        cur.next=rear;
+        return list1;
     }
 }
