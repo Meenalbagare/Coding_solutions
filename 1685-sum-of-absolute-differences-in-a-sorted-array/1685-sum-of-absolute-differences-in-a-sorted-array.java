62d696905f6dae12279d29f8cea8dde247f24778
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-08-05 10:30:15 +0530

    Time: 3 ms (98.43%), Space: 56.4 MB (99.00%) - LeetHub

---
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int[] result=new int[nums.length];
+        int totalS=0;
+        for(int num:nums){
+            totalS+=num;
+        }
+        int leftS=0;
+        for(int i=0;i<nums.length;i++){
+            int rightS=totalS-leftS-nums[i];
+            result[i]=nums[i]*i-leftS+rightS-nums[i]*(nums.length-i-1);
+            leftS+=nums[i];
+        }
+        
+        return result;
+//         int[] result=new int[nums.length];
+        
+//         for(int i=0;i<nums.length;i++){
+//             int j=0;
+//             while(0<=j && j<nums.length){
+//                 if(j!=i){
+//                     result[i]+=Math.abs(nums[i]-nums[j]);
+//                 }
+//                 j++;
+//             }
+           
+//         }
+//         return result;
+        
+    }      
+}
