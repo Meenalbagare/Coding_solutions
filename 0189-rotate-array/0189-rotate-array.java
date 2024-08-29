da31f65b440f19eaf2293f43084450e3bbc7ddab
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-08-29 15:56:31 +0530

    Time: 0 ms (100.00%), Space: 57.1 MB (75.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,17 +1,17 @@
 class Solution {
-    public void reverse(int [] nums,int l,int r){
+    public void reverse(int[] nums,int l,int r){
         while(l<r){
             int temp=nums[l];
             nums[l]=nums[r];
             nums[r]=temp;
-            l+=1;
-            r-=1;
+            l++;
+            r--;
         }
     }
     public void rotate(int[] nums, int k) {
-        k=k%nums.length;
         int l=0;
         int r=nums.length-1;
+        k=k%nums.length;
         reverse(nums,l,r);
         
         l=0;
@@ -21,5 +21,6 @@ class Solution {
         l=k;
         r=nums.length-1;
         reverse(nums,l,r);
+        
     }
 }
