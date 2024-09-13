38d739dec66872992a654384fe746586700dbe46
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-09-13 19:41:42 +0530

    Time: 0 ms (100.00%), Space: 42 MB (54.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -2,34 +2,28 @@ class Solution {
     public int search(int[] nums, int target) {
         int s=0;
         int e=nums.length-1;
-        return searchArray(nums,target,s,e);
+        return searching(nums,target,s,e);
+    }
+    public int searching(int[] nums,int target,int s,int e){
+        if(s>e) return -1;
+        int mid=(s+e)/2;
+        if(target==nums[mid]) return mid;
+        if(nums[s]<=nums[mid]){
+            if(nums[s]<=target && target<=nums[mid]){
+                return searching(nums,target,s,mid-1);
+            } else{
+                return searching(nums,target,mid+1,e);
+            }
+        }
+        else{
+            if(nums[mid]<=target && target<=nums[e]){
+                return searching(nums,target,mid+1,e);
+            } else{
+                return searching(nums,target,s,mid-1);
+            }
+        }
+        
     }
     
-    public static int searchArray(int[] nums,int target, int s, int e){
-        if(s>e){
-            return -1;
-        }
-        int mid=s+(e-s)/2;
-        if(target == nums[mid]){
-            return mid;
-        }
-        
-        if (nums[s] <=nums[mid]) {
-            if (nums[s] <= target && target <= nums[mid]){
-                return searchArray (nums,target,s,mid-1);
-            }
-            else {
-                return searchArray (nums,target,mid+1,e);
-            }
-        }
-        
-        else {
-            if (nums[mid] <= target && target <= nums[e]) {
-                return searchArray(nums,target,mid+1,e);
-            }
-            else {
-                return searchArray (nums,target, s,mid-1);
-            }
-        }
-    }
+    
 }
