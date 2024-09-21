f74fb4775f7fa0f4ef6cf83c53ad9324313cb825
Meenal Bagare <99323366+Meenalbagare@users.noreply.github.com>
2024-09-21 22:20:50 +0530

    Time: 0 ms (100.00%), Space: 43 MB (13.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,14 @@
 class Solution {
     public double power(double x, int n){
-        if(n==0){
-            return 1;
-        }
-        
+        if(n==0) return 1;
         double half=power(x,n/2);
         if(n%2==0){
             return half*half;
         } else{
             return half*half*x;
         }
-        
-        
     }
-    public double myPow(double x, int n) {
+    public double myPow(double x, int n){
         if(n<0){
             x=1/x;
             n=-n;
